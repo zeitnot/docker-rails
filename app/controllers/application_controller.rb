febc557808fa40964e7d200a3dfa5b2abc4f4672
Mehmet Yildiz <mehmet.iu@gmail.com>
2019-06-08 19:33:12 +0300

    add health check for rails image in order to do zero downtime deployment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def healthz
+    head(:ok)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'polls#index'
 
+  get 'healthz' => 'application#healthz'
+
   resources :polls, only: :index do
     post :vote, on: :member
   end
